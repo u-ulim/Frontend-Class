6544951a3cbbf7c8bef8c67e03238d2c090cffb2
u-ulim <uiissoop@gmail.com>
2024-07-29 19:49:53 +0900

    240729

---
+++ 240729/01js/script.js
@@ -0,0 +1,44 @@
+const test1 = "mon";
+
+/******* 호이스팅 *******/
+// var x = 10;
+// var z = x + y;
+
+// var y = 20;
+// console.log(z); => NAN
+
+// const x = 10;
+// const z = x + y;
+
+// const y = 20;
+// console.log(z); => ERROR
+
+/******* 자료형 *******/
+// const num1 = 1;
+// const num2 = 2;
+
+// const sum = num1 + num2;
+// console.log(sum);
+
+// ----------------------
+// 변수안의 할당된 값의 자료형태를 확인할 수 있도록 해주는 함수 =  typeof()
+// ----------------------
+// const str1 = "1";
+// const str2 = "2";
+
+// ----------------------
+
+// ----------------------
+// const num = 1000;
+// const str = "원";
+
+// const counter = num + str;
+// console.log(counter);
+
+// const num1 = 1000;
+// const str1 = "2000";
+
+// const counter1 =  num1 + str1;
+// console.log(counter1);
+
+// 숫자와 문자를 결합하면, 문자가 우선권 => 형변환이라고 한다.
